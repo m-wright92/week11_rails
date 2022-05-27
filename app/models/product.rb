a0e68aeb0091fc78646bad659d7704ea246f2233
Mike Wright <m.wright92@icloud.com>
2022-05-27 11:21:53 -0700

    build most_review scope and spec, pass test

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,13 @@
 class Product < ApplicationRecord
-  scope :most_reviews, -> { }
   has_many :reviews, dependent: :destroy
+  scope :most_reviews, -> {(
+    select("products.id, products.name, products.coo, count(reviews.id) as reviews_count")
+    .joins(:reviews)
+    .group("products.id")
+    .order("reviews_count DESC")
+    .limit(1)
+  )}
+  scope :three_recent, -> { order(created_at: :desc).limit(3)}
   validates :name, presence: true
   validates :coo, presence: true
   validates :cost, presence: true
@@ -14,4 +21,4 @@ class Product < ApplicationRecord
   def coo_upcase
     self.coo = self.coo.upcase
   end
-endgit
+end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -6,15 +6,25 @@ describe Product do
   it { should validate_presence_of :coo }
   it { should validate_presence_of :cost }
   describe '.titleize_product' do
-    it("titleizes the name of a product") do
+    it "titleizes the name of a product" do
       product = Product.create({name: "big huge shoes", coo: "usa", cost: 15})
       expect(product.name).to(eq("Big Huge Shoes"))
     end
   end
   describe '.coo_upcase' do
-    it("upcases the coo before save") do
+    it "upcases the coo before save" do
       product = Product.create({name: "big huge shoes", coo: "usa", cost: 15})
       expect(product.coo).to(eq("USA"))
+    end
+  end
+  describe '.most_reviews' do
+    it "returns the most reviewed product" do
+      product = Product.create({name: "big huge shoes", coo: "usa", cost: 15, id: nil})
+      product.save
+      product2 = Product.create({name: "little tiny shoes", coo: "usa", cost: 15, id: nil})
+      product2.save
+      review = Review.create({author: "mike", content_body: "abcdefighijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz", rating: 3, product_id: product.id})
+      expect(Product.most_reviews).to(eq([product]))
     end
   end
 end
