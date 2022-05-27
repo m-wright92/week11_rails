94fbfd84bca2eaa46f8d28b120d2695e68a96e29
Mike Wright <m.wright92@icloud.com>
2022-05-27 15:37:01 -0700

    add view pages

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,7 @@
 class Product < ApplicationRecord
   has_many :reviews, dependent: :destroy
+  scope :three_recent, -> { order(created_at: :desc).limit(3)}
+  scope :usa, -> { where(coo: "USA") }
   scope :most_reviews, -> {(
     select("products.id, products.name, products.coo, count(reviews.id) as reviews_count")
     .joins(:reviews)
@@ -7,8 +9,7 @@ class Product < ApplicationRecord
     .order("reviews_count DESC")
     .limit(1)
   )}
-  scope :three_recent, -> { order(created_at: :desc).limit(3)}
-  scope :usa, -> { where(coo: "USA") }
+
   validates :name, presence: true
   validates :coo, presence: true
   validates_length_of :coo, is: 3
