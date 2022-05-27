d974ef07c20b5a9412453032d88c1b558b91af24
Mike Wright <m.wright92@icloud.com>
2022-05-27 14:35:00 -0700

    setup products controller

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,53 @@
+class ProductsController < ApplicationController
+
+  def index
+    @products = Product.all
+    render :index
+  end
+
+  def new
+    @product = Product.new
+    render :new
+  end
+
+  def create
+    @product = Product.new(division_params)
+    if @product.save
+      redirect_to products_path
+    else 
+      render :new
+    end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+    render :edit
+  end
+
+  def show
+    @products = Product.find(params[:id])
+    render :show
+  end
+
+  def update
+    @products = Product.find(params[:id])
+    if @products.update(products_params)
+      flash[:notice] = "Product Updated"
+      redirect_to products_path
+    else
+      render :edit  
+    end
+  end
+
+  def destroy
+   @product = Product.find(params[:id])
+   @product.destroy
+   flash[:notice] = "Product Deleted"
+   redirect_to products_path
+  end
+
+  private
+    def division_params
+      params.require(:product).permit(:name)
+    end
+end
