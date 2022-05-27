fd22dcc2fa0ce5b5e09d46506649ba46579266d9
Mike Wright <m.wright92@icloud.com>
2022-05-27 10:11:57 -0700

    pass fifth test

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -5,4 +5,8 @@ describe Product do
   it { should validate_presence_of :name }
   it { should validate_presence_of :coo }
   it { should validate_presence_of :cost }
+  it("titleizes the name of a product") do
+    product = Product.create({name: "big huge shoes", coo: "usa", cost: 15})
+    expect(product.name).to(eq("Big Huge Shoes"))
+  end
 end
