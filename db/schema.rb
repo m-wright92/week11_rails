5845eb01045c3c1bacaaa619aa7e8f043e565671
Mike Wright <m.wright92@icloud.com>
2022-05-27 09:22:35 -0700

    add routes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_27_155139) do
+ActiveRecord::Schema.define(version: 2022_05_27_161000) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,8 +27,10 @@ ActiveRecord::Schema.define(version: 2022_05_27_155139) do
     t.string "author"
     t.string "content_body"
     t.integer "rating"
+    t.integer "product_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "reviews", "products"
 end
