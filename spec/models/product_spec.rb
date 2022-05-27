e71a84b38e6a0b9613001eea903d232f98ad7fa9
Mike Wright <m.wright92@icloud.com>
2022-05-27 10:01:28 -0700

    pass fourth test

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -4,4 +4,5 @@ describe Product do
   it { should have_many :reviews }
   it { should validate_presence_of :name }
   it { should validate_presence_of :coo }
+  it { should validate_presence_of :cost }
 end
