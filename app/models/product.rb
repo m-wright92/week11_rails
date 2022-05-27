fe793d637580b0e2f66157748996b89481fb8eb8
Mike Wright <m.wright92@icloud.com>
2022-05-27 09:58:20 -0700

    pass second test

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,12 @@
 class Product < ApplicationRecord
   has_many :reviews, dependent: :destroy
+  validates :name, presence: true
+  validates :coo, presence: true
+  validates :cost, presence: true
+  before_save :titleize_product
+
+  private
+  def titleize_product
+    self.name = self.name.titleize
+  end
 end
