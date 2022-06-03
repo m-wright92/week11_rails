93c9b0ae58dc0a9c981d6ead76556db7d9b451e3
Mike Wright <m.wright92@icloud.com>
2022-06-03 10:42:04 -0700

    build seed with proper relations

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,18 @@
 Product.destroy_all
+Review.destroy_all
 
 50.times do |index|
   Product.create!(name: Faker::Food.ingredient,
-                  cost: Faker::Commerce.price(range: 0..20.0, as_string: true),
+                  cost: Faker::Commerce.price(range: 0..20.0, as_string: false),
                   coo: Faker::Address.country_code_long)
 end
 
-p "Created #{Product.count} products"
-
-Review.destroy_all
 
 250.times do |index|
   Review.create!(author: Faker::GreekPhilosophers.name,
-                content_body: Faker::Lorem.paragraph_by_chars(number: 50..250, supplemental: false),
+                content_body: Faker::Lorem.paragraph_by_chars(number: 80, supplemental: false),
                 rating: Faker::Number.between(from: 1, to: 5),
-                product_id: )
+                product_id: rand(Product.ids.first..Product.ids.last) )
 end
 
-params = { product: { name: Faker::Food.ingredient, }}
+p "Created #{Product.count} products and #{Review.count} reviews"
