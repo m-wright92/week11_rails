5ccc71053865141558c12947d8867c4f215599fa
Mike Wright <m.wright92@icloud.com>
2022-06-03 09:32:31 -0700

    update products with price

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ Product.destroy_all
 
 50.times do |index|
   Product.create!(name: Faker::Food.ingredient,
-                  cost: Faker::Number.decimal(l_digits: 2),
+                  cost: Faker::Commerce.price(range: 0..20.0, as_string: true),
                   coo: Faker::Address.country_code_long)
 end
 
